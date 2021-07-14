9a2f69c1c10f6747209b0baf4945fc673be38a81
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-14 20:15:43 +0700

    menambahkan stack Navigation

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
+import MealsNavigator from './navigation/MealsNavigator';
+
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -23,21 +25,6 @@ export default function App() {
     )
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: 'center'
-  },
-  text: {
-    fontFamily: 'open-sans-bold'
-  }
-});
 
