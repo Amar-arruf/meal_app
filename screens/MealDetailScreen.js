1e6a5894f0b9d5b7bc38b3cfdaa7fe8d6338497f
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-15 11:11:03 +0700

    menambahkan header title pada layar Detail

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button } from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailScreen = props => {
   return (
     <View style={styles.screen}> 
@@ -12,6 +14,15 @@ const MealDetailScreen = props => {
         }} />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  return {
+    headerTitle : selectedMeal.title
+  };
 };
 
 const styles = StyleSheet.create({
