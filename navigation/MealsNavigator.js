f857f46a65531f4b9b9418417aaafea20247da7c
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-19 15:10:14 +0700

    menambahkan navigation tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import {createAppContainer} from 'react-navigation'
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoriteScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator(
@@ -28,4 +32,43 @@ const MealsNavigator = createStackNavigator(
   }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons 
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: 'Favorites!',
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons 
+              name="ios-star" 
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor
+    }
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
