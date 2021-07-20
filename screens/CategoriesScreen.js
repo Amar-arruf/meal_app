0c963c6cc218c8397e3b7804a44c8ea3e1575b95
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-20 08:50:25 +0700

    menambahkan toggle menu drawer

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList, Platform, TouchableOpacity } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import CustomHeaderButtons from '../components/HeaderButton';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
@@ -31,8 +33,21 @@ const CategoriesScreen = props => {
   );
 };
 
-CategoriesScreen.navigationOptions = {
-  headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions =  navData => {
+  return {
+    headerTitle: 'Meal Categories',
+    headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButtons}>
+      <Item 
+        title="Menu"
+        iconName="ios-menu"
+        onPress={() => {
+          navData.navigation.toggleDrawer();
+        }}
+      />
+    </HeaderButtons>
+  )
+  };
 };
 
 const styles = StyleSheet.create({
