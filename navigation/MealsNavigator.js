9798f66e4c61f382a3a02f8b17f5b70767f4cd1d
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-20 09:13:20 +0700

    menambahkan custom components Default Text

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -18,6 +18,12 @@ import Colors from '../constants/Colors';
 const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor: ''
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
   },
   headerTintColor: 
     Platform.OS === 'android' ? 'white' : Colors.primaryColor,
@@ -58,7 +64,13 @@ const tabScreenConfig = {
           <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor}/>
         );
       },
-      tabBarColor: Colors.accentColor
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: 
+        Platform.OS === 'android' ? (
+          <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text>
+        ) : (
+          'Meals'
+        )
     }
   },
   Favorites: {
@@ -70,7 +82,13 @@ const tabScreenConfig = {
           />
         );
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: 
+      Platform.OS === 'android' ? (
+        <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text>
+      ) : (
+        'Favorites'
+      )
     }
   }
 }
@@ -87,6 +105,9 @@ const MealsFavTabNavigator =
   })
   : createBottomTabNavigator(tabScreenConfig,{
     tabBarOptions: {
+      labelStyle: {
+        fontFamily: 'open-sans'
+      },
       activeTintColor: Colors.accentColor
     }
   });
